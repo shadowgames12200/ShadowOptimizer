4f8ffec31029361a716f0f6332ace7b7cb0be04a
Charles <charles@example.com>
2026-07-15 22:53:37 +0000

    Fix: Integrate authRouter into appRouter

--- server/routers.ts
+++ server/routers.ts
@@ -8,6 +8,7 @@ import { createLicenseProcedure, revokeLicenseProcedure, deleteLicenseProcedure 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
   system: systemRouter,
+  auth: authRouter,
 
 
   // Licensing system routers
